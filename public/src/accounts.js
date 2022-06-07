41041eaadd452058b1bd4c9144cc9d49efd54da2
Kalif Ahmed <97050072+kalifthedeveloper@users.noreply.github.com>
2022-06-07 11:18:04 -0500

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,33 +1,53 @@
 function findAccountById(accounts, id) {
-  return accounts.find((account) => account.id === id);
+  return accounts.find((account) => account.id.includes(id));
+}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((lastA,lastB) => 
+    lastA.name.last.toLowerCase() > lastB.name.last.toLowerCase() ? 1 : -1
+  );
 }
 
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((accountA, accountB) =>
-    accountA.name.last > accountB.name.last ? 1 : -1
+  return accounts.sort((lastA,lastB) => 
+    lastA.name.last.toLowerCase() > lastB.name.last.toLowerCase() ? 1 : -1
   );
 }
+  
 
 function getTotalNumberOfBorrows(account, books) {
-  return books.filter((book) =>
-    book.borrows.find((borrow) => borrow.id === account.id)
-  ).length;
+  let result = 0;
+  const booksBorrowedByAccount = books.forEach((book) => {
+    if (!!book.borrows) {
+      book.borrows.forEach((accounts) => {
+        if (accounts.id === account.id) {
+          result++;
+        }
+      });
+    }
+  });
+  return result;
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-  const borrowedBooks = books.filter((book) =>
-    book.borrows.some((borrow) => {
-      if (borrow.id !== account.id) return false;
-      if (!borrow.returned) {
-        return true;
+  const borrowedBooks = [];
+  books.forEach((book) => {
+    let bookBorrows = book.borrows;
+    bookBorrows.forEach((borrow) => {
+      if (borrow.id === account.id && !borrow.returned) {
+        borrowedBooks.push(book);
       }
-    })
-  );
-  borrowedBooks.forEach(
-    (book) =>
-      (book.author = authors.find((author) => book.authorId === author.id))
-  );
-  return borrowedBooks;
+    });
+  });
+  let result = borrowedBooks.map((book) => {
+    return { ...book, author: getAuthor(book, authors) };
+  });
+  return result;
+}
+// Helper function
+// Returns author object
+function getAuthor(book, authors) {
+  const author = authors.find((author) => author.id === book.authorId);
+  return author;
 }
 
 module.exports = {
